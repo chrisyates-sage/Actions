1a7b63bd988087ec9bb523566ac47ca696bd41ba
Chris Yates <chris.yates@sage.com>
2019-11-01 16:38:58 +0000

    Initial commit of node modules for ms-teams-notify

---
+++ ms-teams-notify/index.js
@@ -0,0 +1,64 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+	  //core.setOutput("time", time);
+	  const payload = JSON.stringify(github.context.payload, undefined, 2)
+	  //const mymsg = core.getInput('message')
+	  jsondata = new Object()
+	  jsondata.text = `_${process.env.GITHUB_WORKFLOW}_ :- New build run from ${process.env.GITHUB_EVENT_NAME} event by`
+	  jsondata.title = `triggered from ${process.env.GITHUB_REPOSITORY}`
+
+	  var action1 = {
+		'@context': 'http://schema.org',
+		'target': [github.context.payload.compare],
+		'@type': 'ViewAction',
+		'name': 'Go to repo'	
+	  }
+	  var fact1 = {
+		'name': 'Repository',
+		'value': process.env.GITHUB_REPOSITORY
+	  }
+	  var fact2 = {
+		'name': 'Commit SHA',
+		'value': process.env.GITHUB_SHA
+	  }
+	  jsondata['potentialAction'] = []
+	  jsondata['potentialAction'].push(action1)
+	  
+	  var section1 = new Object()
+	  jsondata['sections'] = []
+	  section1['facts'] = []
+	  section1['facts'].push(fact1)
+	  section1['facts'].push(fact2)
+	  section1['activityImage'] = `https://github.com/${process.env.GITHUB_ACTOR}.png?size=48`
+	  section1['activityText'] = github.context.payload.head_commit.message
+	  section1['activityTitle'] = process.env.GITHUB_ACTOR
+	  jsondata['sections'].push(section1)
+	  
+	  const jsonout = JSON.stringify(jsondata, undefined, 2)
+	  //console.log(jsonout)
+
+	  //var url = "https://outlook.office.com/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef";
+	  const url = core.getInput('teamsurl');
+	  console.log(`Url passed: ${url}`)
+
+	  const https = require('https');
+      var options = {
+	   hostname: 'outlook.office.com',
+	   path: '/webhook/4debbfbb-c394-4de0-a0eb-3cd444c9554e@3e32dd7c-41f6-492d-a1a3-c58eb02cf4f8/IncomingWebhook/2aac373336894c12b3db5d43c350af17/2b82dd20-1338-4397-807d-e05538020cef',
+	   method: 'POST',
+	   headers: { 'Content-Type': 'application/json', "Content-Length": jsonout.length }
+	  };
+	  var req = https.request(options, (res) => {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:', res.headers);
+	  	res.on('data', (d) => {  process.stdout.write(d);  });
+	  });
+	  req.on('error', (e) => {  console.error(e);  });
+	  req.write(jsonout);
+	  req.end();
+
+} catch (error) {
+	  core.setFailed(error.message);
+}
